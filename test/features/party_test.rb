6886a33b717da7f88a7719091776c653565ddd26
Christian Kyony <ckyony@changamuka.com>
2016-03-09 11:27:39 +0200

    Use sign_in_as helper to test Party feature

--- test/features/party_test.rb
+++ test/features/party_test.rb
@@ -1,28 +1,21 @@
 require 'test_helper'
 
 feature 'Party' do
+  let(:john) { users(:john)  }
+
   before do
-    visit root_path
-    click_link 'Sign up' 
-    fill_in 'Name' , :with => 'john' 
-    fill_in 'Email' , :with => 'kyony@gmail.com' 
-    fill_in 'Password' , :with => 'johnte12'  
-    fill_in 'Password confirmation' , :with => 'johnte12' 
-    click_button 'Sign up' 
+    sign_in_as john
   end
-  
+
   scenario 'Current user must be able to create a new Event' do
     click_link 'New Event'
-    assert_content 'Please type in details of your new event'
-
-
-    fill_in 'Name' , :with => 'New Year Eve Party' 
-    fill_in 'Location' , :with => 'Far, Far, Far Away' 
-    select "2016", :from=> 'party_occurence_1i'
-    select "December", :from=> 'party_occurence_2i'
-    select "31", :from=> 'party_occurence_3i'
-    select "20", :from=> 'party_occurence_4i'
-    select "00", :from=> 'party_occurence_5i'
+    fill_in 'Name' , with: 'New Year Eve Party' 
+    fill_in 'Location' , with:  'Far, Far, Far Away' 
+    select '2016', from:  'party_occurence_1i'
+    select 'December', from:  'party_occurence_2i'
+    select '31', from:  'party_occurence_3i'
+    select '20', from:  'party_occurence_4i'
+    select '00', from:  'party_occurence_5i'
     click_button 'Save'
 
     assert_content 'A new Event has been successfully created.'
@@ -33,14 +26,8 @@ feature 'Party' do
 
   scenario 'Current user must be able to see all the parties he has created' do
     skip
-
-    page.assert_text 'Parties' 
     click_link 'Parties'
-    visit parties_path
 
-    page.must_have_content 'Name'
-    page.must_have_content  'Location'
-    page.must_have_content 'Password'
   end
 
   scenario 'Current user should be able to add guest to party ' do      
