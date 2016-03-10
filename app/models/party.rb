ad3918b14034013a0768b128d28cb31f47910668
john kyony <jkyony@gmail.com>
2016-03-10 12:28:18 +0100

    The model test

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,8 @@
 class Party < ActiveRecord::Base
   belongs_to :user
   has_many :guests
+  validates :user, presence: true
+  validates :location , presence: true
+  validates :occurence , presence: true
+  
 end
--- test/models/party_test.rb
+++ test/models/party_test.rb
@@ -3,7 +3,11 @@ require "test_helper"
 describe Party do
   let(:party) { Party.new }
 
-  it "must be valid" do
-    value(party).must_be :valid?
+  it "The party variable must be present" do
+    # value(party).must_be :valid? 
+    value(party.name).must_be :present?
+    value(party.location).must_be :present?
+    value(party.occurence).must_be :present?
+    value(party.user).must_be :present?
   end
 end
