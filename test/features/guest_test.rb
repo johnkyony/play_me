eed1e5a60aac98709e01996b57f7d79066c542a0
john kyony <jkyony@gmail.com>
2016-03-15 12:26:31 +0100

    the test

--- test/features/guest_test.rb
+++ test/features/guest_test.rb
@@ -1,39 +1,16 @@
 require "test_helper"
 
 feature "Guest" do
-  before do
-    skip
-    visit root_path
-    click_link("Sign up")
-    fill_in('Name' , :with => "john")
-    fill_in('Email' , :with => "kyony@gmail.com")
-    fill_in('Password' , :with => "johnte12" )
-    fill_in('Password confirmation' , :with => "johnte12")
-    click_button('Sign up')
-    @guest = guests(:cool_guy)
-  end
-
-  scenario "When guest invited to a party should be able to see the party details" do
-    skip
-    visit root_path
-  end
-
-  scenario "When guest invited to a party should be able to see the party details" do
-    skip     
-    click_link 'Parties'   
-    click_link('Invitation')
-    # visit('#Invitation')
-    # page.must_have_content @guest.party.name
-    click_link(@guest.party.name)
-    # visit party_path(@guest.party)
-    page.must_have_content @guest.party.name
-    page.must_have_content @guest.party.location
-    page.must_have_content @guest.party.password
-  end 
+  let(:john) { users(:john)  }
+  let(:lena) { users(:lena) }
+  let(:glen) { users(:glen) }
+  
+ 
+  
 
  
 
-  scenario "If invited to party the should should be able to invite his friend" do
+  scenario "" do
     skip
     visit root_path
     page.assert_text("Parties")
