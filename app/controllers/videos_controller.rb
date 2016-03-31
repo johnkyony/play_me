d2590d8becb981a4b9470054e97e7907ed3a9a8d
john kyony <jkyony@gmail.com>
2016-03-31 20:36:51 +0100

    Youtube filter added

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,7 +2,7 @@ class VideosController < ApplicationController
 
   def index
     client = Yt::Collections::Videos.new
-    @videos = client.where(q: params[:query])
+    @videos = client.where(q: params[:query] ,  videoCategoryId: "10" )
   end
     
 end
