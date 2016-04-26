77542cec82b0a232294bb372ffb690995e08d222
john kyony <jkyony@gmail.com>
2016-04-26 19:15:14 +0100

    Finish track search on database

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,12 +1,15 @@
 class TracksController < ApplicationController
-  require 'soundcloud'
+ 
 
   def index
-    client = Soundcloud.new(:client_id => '6d4b8e66ddd26d1f7df54714074db7aa')
-    if params[:query]
-      @tracks = client.get('/tracks', :q => params[:query] , :limit => 200 )
+    @tracks = Track.all
+    if params[:search]
+      @tracks = Track.search(params[:search]).order("created_at Desc")
+      
     else
-      @tracks = client.get('/tracks', :limit => 100, :order => 'hotness')
+      @tracks = Track.all.order('created_at DESC')
     end
+
   end
+  
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,10 @@
 class Track < ActiveRecord::Base
   has_many :playlist
+
+  def self.search(search)
+    where("name LIKE ?" , "%#{search}%")
+    
+  end
+
+
 end
